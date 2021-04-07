2f9143bcdbb95f709e5cd9014f5b9ce6cafe1427
smiley717 <smiley.dev717@gmail.com>
2021-04-07 09:31:38 +0200

    style: modify ui to upload file.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import './App.css';
 import React, { useState } from 'react'
 import './App.css'
 import Amplify, { Storage } from 'aws-amplify'
+import Container from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import { InputLabel } from '@material-ui/core';
+
 import {
   AmplifyAuthenticator,
   AmplifySignOut,
   AmplifySignIn,
   AmplifySignUp,
+  withAuthenticator
 } from '@aws-amplify/ui-react'
-import { MdSend /* MdList */ } from 'react-icons/md'
-import awsConfig from './aws-exports'
-Amplify.configure(awsConfig)
+import './aws-exports'
+// Amplify.configure(awsConfig)
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
   const [name, setName] = useState('')
   const [file, setFile] = useState('')
   const [response, setResponse] = useState('')
@@ -24,6 +39,7 @@ function App() {
       setName(e.target.files[0].name)
     }
   }
+
   const onSubmit = (e) => {
     e.preventDefault()
     if (file) {
@@ -49,27 +65,34 @@ function App() {
   }
   return (
     <div className="App">
+      <Container >
       <div className='video-uploader'>
         <form onSubmit={(e) => onSubmit(e)}>
-          <p>
-            <label className='select-label'>Select video: </label>
-          </p>
-          <p>
-            <input
-              className='video-input'
-              type='file'
-              id='file-input'
-              accept='image/*, video/*'
-              onChange={(e) => onChange(e)}
-            />
-          </p>
-          <button type='submit' className='btn'>
-            Submit <MdSend className='btn-icon' />
-          </button>
+          <InputLabel htmlFor="file-input"><h1>Select video:</h1></InputLabel>
+
+          <OutlinedInput
+            className='video-input'
+            type='file'
+            id='file-input'
+            accept='image/*, video/*'
+            onChange={(e) => onChange(e)}
+          />
+          <Button
+            variant="contained"
+            type='submit'
+            color="secondary"
+            className={classes.button}
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload
+          </Button>
         </form>
+        <h1>{ response }</h1>
       </div>
+      <AmplifySignOut/>
+      </Container>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -20,41 +20,41 @@ Amplify.configure({
         userPoolWebClientId: '7onm92ophpgh2j3l1dbl1qt0h9',
 
         // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-        mandatorySignIn: false,
+        // mandatorySignIn: false,
 
         // OPTIONAL - Configuration for cookie storage
         // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
-        cookieStorage: {
-            // REQUIRED - Cookie domain (only required if cookieStorage is provided)
-            domain: '.yourdomain.com',
-            // OPTIONAL - Cookie path
-            path: '/',
-            // OPTIONAL - Cookie expiration in days
-            expires: 365,
-            // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
-            sameSite: "strict" | "lax",
-            // OPTIONAL - Cookie secure flag
-            // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
-            secure: true
-        },
+        // cookieStorage: {
+        //     // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+        //     domain: '.yourdomain.com',
+        //     // OPTIONAL - Cookie path
+        //     path: '/',
+        //     // OPTIONAL - Cookie expiration in days
+        //     expires: 365,
+        //     // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+        //     sameSite: "strict" | "lax",
+        //     // OPTIONAL - Cookie secure flag
+        //     // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+        //     secure: true
+        // },
 
         // OPTIONAL - customized storage object
         // storage: MyStorage,
 
         // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-        authenticationFlowType: 'USER_PASSWORD_AUTH',
+        // authenticationFlowType: 'USER_PASSWORD_AUTH',
 
         // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
-        clientMetadata: { myCustomKey: 'myCustomValue' },
+        // clientMetadata: { myCustomKey: 'myCustomValue' },
 
         // OPTIONAL - Hosted UI configuration
-        oauth: {
-            domain: 'your_cognito_domain',
-            scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
-            redirectSignIn: 'http://localhost:3000/',
-            redirectSignOut: 'http://localhost:3000/',
-            responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
-        }
+        // oauth: {
+        //     domain: 'your_cognito_domain',
+        //     scope: ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+        //     redirectSignIn: 'http://localhost:3000/',
+        //     redirectSignOut: 'http://localhost:3000/',
+        //     responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+        // }
     },
     Storage: {
         AWSS3: {
